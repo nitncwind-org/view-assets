ae40036a8b972d9190ef5b0850053dd72e631680
とろんぽっと.nara <NAKNAO-nnct@users.noreply.github.com>
2020-09-30 02:06:39 +0900

    fix: ファイル名の変更

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 wget -O about.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=about &&
 wget -O concerts.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=concerts &&
-wget -O ensemble.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=winds &&
+wget -O winds.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=winds &&
 wget -O ensemble.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=ensemble &&
 wget -O link.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=link &&
 wget -O news.csv https://script.google.com/macros/s/AKfycbxl_iWjgcunOEu0tLvKlnLC4MM9CpcsmNu_L_O1yHWp1x2XHnWN/exec?key=news
